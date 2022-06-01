5b79750b02ee91f6df8242ea2a301f4384f1ec5e
r134x7 <r134x7@gmx.com>
2022-06-01 21:46:32 +1000

    fixed signup issue which created 400 error

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,18 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
+  const userName = document.querySelector("#name-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  console.log(name);
+  console.log(userName);
   console.log(email);
   console.log(password);
 
-  if (name && email && password) {
+  if (userName && email && password) {
     const response = await fetch("/api/users", {
       method: "POST",
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ userName, email, password }),
       headers: { "Content-Type": "application/json" },
     });
 

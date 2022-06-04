1971552cf0f9f085b15b1896d432257c10cdcae1
r134x7 <r134x7@gmx.com>
2022-06-04 19:52:34 +1000

    fixed labels, added sort, changed bar graph to stacked bar graph

--- public/js/distancechart.js
+++ public/js/distancechart.js
@@ -1,83 +1,76 @@
-/*
-only have time and distance
-
-can make charts for....
-
-users vs total distance
-
-users vs m/s
-
-users vs ...
-*/
-
-// const { options } = require("../../controllers"); this was not supposed to appear but appeared because of typing in options for the chart settings
-
-// const Chart = require("chart.js");
-// ../../node_modules/chart.js/types/index.esm
 const postId = [];
 const userName = [];
 const distance = [];
 const colour = [];
+const xyCoordinates = [];
 
 for (let index = 1; index < 1000; index++) {
-
+  // gets postId and pushes the container's data to the arrays used
   const getPostId = document.querySelector(`.box${index}`);
 
   if (getPostId !== null) {
     postId.push(document.querySelector(`.box${index}`).getAttribute("data-id"));
 
-    const getUserName = userName.push(document.querySelector(`.user${index}`).getAttribute("data-id"));
+    const getUserName = userName.push(
+      document.querySelector(`.user${index}`).getAttribute("data-id")
+    );
 
-    const getdistance = distance.push(document.querySelector(`.content${index}`).getAttribute("data-id"));
-
-    // const getColour = Math.floor(Math.random()
+    const getdistance = distance.push(
+      document.querySelector(`.content${index}`).getAttribute("data-id")
+    );
+    // generating random rgba colours
     const getColour1 = Math.floor(Math.random() * 256);
     const getColour2 = Math.floor(Math.random() * 256);
     const getColour3 = Math.floor(Math.random() * 256);
 
-    colour.push(`rgba(${getColour1}, ${getColour2}, ${getColour3}, 0.8)`)
-
+    colour.push(`rgba(${getColour1}, ${getColour2}, ${getColour3}, 0.8)`);
   }
-
 }
 
+for (let index = 0; index < distance.length; index++) {
+  // taking the xy co-ordinates and making objects to push into an array
+  const test = {
+    y: userName[index],
+    x: distance[index],
+  };
 
-// const postId2 = box.push(document.querySelector(".box").getAttribute("{{post.runName}}"));
+  xyCoordinates.push(test);
+}
 
-// console.log(box);
+xyCoordinates.sort((a, b) => b.x - a.x); // sorting the objects in the array in descending order. Source: https://stackoverflow.com/questions/979256/sorting-an-array-of-objects-by-property-values
 
-
-let distanceChart = document.querySelector("#distanceChart").getContext("2d");
-
+let distanceChart = document.querySelector("#distanceChart").getContext("2d"); // selecting the html canvas and to render in 2D
 let barChart = new Chart(distanceChart, {
-  type:"bar",
+  // creating a stacked bar chart
+  type: "bar",
   data: {
-    labels: userName, // x-axis labels
-    datasets: [{
-      axis: 'y',
-      label: 'Distance chart in km for x date',
-      data: distance, // y-axis labels
-      backgroundColor: colour,
-    borderWidth: 1,
-    borderColor: 'black',
-    }],
+    datasets: [
+      {
+        axis: "y",
+        label: "Distance in km by posted run",
+        data: xyCoordinates,
+        backgroundColor: colour,
+        borderWidth: 1,
+        borderColor: "black",
+      },
+    ],
   },
   options: {
-    indexAxis: 'y',
-  }
-})
-
-// let barChart2 = new Chart(distanceChart, {
-//   type:"bar",
-//   data: {
-//     labels: userName, // x-axis labels
-//     datasets: [{
-//       label: 'Distance chart in km for x date',
-//       data: distance, // y-axis labels
-//       backgroundColor: colour,
-//     borderWidth: 1,
-//     borderColor: 'black',
-//     }],
-//   },
-// })
-
+    indexAxis: "y", // to create horizontal bar graph
+    scales: {
+      // axis labels
+      y: {
+        title: {
+          display: true,
+          text: "User names of posted runs",
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Distance in km",
+        },
+      },
+    },
+  },
+});
